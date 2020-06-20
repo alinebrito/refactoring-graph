c31f72fe5f8ea280c3bf82cc949f2d678cf4de2f
alinebrito <nbrito.aline@gmail.com>
2020-06-20 19:14:23 -0300

    refactor(icons): size of GitHub and logo icons

--- src/Nav.js
+++ src/Nav.js
@@ -5,11 +5,12 @@ export default () => {
   return (
     <header className="navbar navbar-dark bg-dark" >
       <a className="navbar-brand" href="/#">
-        <img src={logo} width="40" height="40" alt=""/>
+        <img src={logo} width="45" height="45" alt=""/>
       </a>
       <div className="justify-content-end">
           <a href="https://github.com/alinebrito/refactoring-graph">
             <img
+              width="100" height="25"
               alt="GitHub stars"
               src="https://img.shields.io/github/stars/alinebrito/refactoring-graph?logo=github&style=social"
             />
