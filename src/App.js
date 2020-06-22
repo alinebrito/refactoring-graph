d47d4750e9d36602ba17a533e027cb12202b89a9
alinebrito <nbrito.aline@gmail.com>
2020-06-22 00:08:16 -0300

    feat(router): add default router

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import './style.css';
 import RefGraphComponent from "./components/RefGraphComponent.js";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import Navbar from './Nav';
 
 function App() { 
@@ -16,9 +16,7 @@ function App() {
               <div id="graphbody">
                 <Switch>
                   <Route path="/:owner/:project/:id" component={RefGraphComponent}/>
-                  <Route path="/:owner" component={RefGraphComponent} />
-                  <Route path="/:owner/:project" component={RefGraphComponent} />
-                  <Route path="/" component={RefGraphComponent} />
+                  <Redirect from="/" exact to="/test/test/1"/>
                 </Switch>
               </div>
             </div>
